823eab6b5957edd1aa7fb421a77459f5efcaf0e7
dimsonson <104103457+dimsonson@users.noreply.github.com>
2022-11-25 16:08:24 +0300

    user handlers test

--- internal/handlers/servicemock/usermock.go
+++ internal/handlers/servicemock/usermock.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"errors"
+	"fmt"
 
 	"github.com/dimsonson/go-yandex-diploma-tpl/internal/models"
 )
@@ -17,14 +18,23 @@ func (msrv *User) Create(ctx context.Context, dc models.DecodeLoginPair) (err er
 	case dc.Login == "dimma" && dc.Password == "12345":
 		return nil
 	case dc.Login == "dimma2login":
-		return errors.New("login existc")
+		return errors.New("login exist")
 	default:
 		return errors.New("something wrong woth server")
 	}
 }
 
 func (msrv *User) CheckAuthorization(ctx context.Context, dc models.DecodeLoginPair) (err error) {
-	return nil
+	fmt.Println(dc.Login, dc.Password)
+	switch {
+	case dc.Login == "dimma" && dc.Password == "12345":
+		return nil
+	case dc.Login == "dimma3login" && dc.Password == "12345":
+		fmt.Println(dc.Login, dc.Password)
+		return errors.New("login or password not exist")
+	default:
+		return errors.New("something wrong woth server")
+	}
 }
 
 // функция SHA.256 хеширования строки и кодирования хеша в строку
--- internal/handlers/tests/usermock_test.go
+++ internal/handlers/tests/usermock_test.go
@@ -22,49 +22,39 @@ func TestHandler_Create(t *testing.T) {
 		inputBody              string
 		expectedStatusCode     int
 		expectedResponseBody   string
-		//expectedHeader1        string
 		expectedHeader2        string
-		//expectedHeaderContent1 string
 		expectedHeaderContent2 string
 	}{
 		// определяем все тесты
 		{
-			name:                   "OK (positive) test for user registration",
+			name:                   "Positive test for user registration",
 			inputMetod:             "POST",
 			inputEndpoint:          "/api/user/register",
 			inputBody:              `{ "login": "dimma", "password": "12345" }`,
 			expectedStatusCode:     http.StatusOK,
-			// expectedHeader1:        "Content-Type",
-			// expectedHeaderContent1: "text/plain",
 			expectedHeader2:        "Authorization",
 			expectedHeaderContent2: "Bearer",
 		},
 		{
-			name:                   "Negative test user registration - wrong JSON",
-			inputMetod:             "POST",
-			inputEndpoint:          "/api/user/register",
-			inputBody:              `{ "login": "dimma, "password": "12345" }`,
-			expectedStatusCode:     http.StatusBadRequest,
-			//expectedHeader1:        "Content-Type",
-			//expectedHeaderContent1: "text/plain; charset=utf-8",
+			name:               "Negative test user registration - wrong JSON",
+			inputMetod:         "POST",
+			inputEndpoint:      "/api/user/register",
+			inputBody:          `{ "login": "dimma, "password": "12345" }`,
+			expectedStatusCode: http.StatusBadRequest,
 		},
 		{
-			name:                   "Negative test user registration - login exist",
-			inputMetod:             "POST",
-			inputEndpoint:          "/api/user/register",
-			inputBody:              `{ "login": "dimma2login", "password": "12345" }`,
-			expectedStatusCode:     http.StatusConflict,
-			//expectedHeader1:        "Content-Type",
-			//expectedHeaderContent1: "text/plain; charset=utf-8",
+			name:               "Negative test user registration - login exist",
+			inputMetod:         "POST",
+			inputEndpoint:      "/api/user/register",
+			inputBody:          `{ "login": "dimma2login", "password": "12345" }`,
+			expectedStatusCode: http.StatusConflict,
 		},
 		{
-			name:                   "Negative test user registration - server error",
-			inputMetod:             "POST",
-			inputEndpoint:          "/api/user/register",
-			inputBody:              `{ "login": "dimmaServErr", "password": "12345" }`,
-			expectedStatusCode:     http.StatusInternalServerError,
-			//expectedHeader1:        "Content-Type",
-			//expectedHeaderContent1: "text/plain; charset=utf-8",
+			name:               "Negative test user registration - server error",
+			inputMetod:         "POST",
+			inputEndpoint:      "/api/user/register",
+			inputBody:          `{ "login": "dimmaServErr", "password": "12345" }`,
+			expectedStatusCode: http.StatusInternalServerError,
 		},
 	}
 	s := &servicemock.User{}
@@ -73,17 +63,86 @@ func TestHandler_Create(t *testing.T) {
 	for _, tCase := range tests {
 		// запускаем каждый тест
 		t.Run(tCase.name, func(t *testing.T) {
-			// Test Server
+			// конфигурирование запроса
 			rout := chi.NewRouter()
 			rout.Post("/api/user/register", h.Create)
 			// запрос
 			request := httptest.NewRequest(tCase.inputMetod, tCase.inputEndpoint, bytes.NewBufferString(tCase.inputBody))
-			// create Test Request
+			/// создание запроса
 			w := httptest.NewRecorder()
-			// perform Test Request
+			// запуск
 			rout.ServeHTTP(w, request)
-			// Assert results
-			// assert.Contains(t, w.Header().Get(tCase.expectedHeader1), tCase.expectedHeaderContent1)
+			// оценка результатов
+			assert.Equal(t, tCase.expectedStatusCode, w.Code)
+			if w.Code == http.StatusOK {
+				assert.Contains(t, w.Header().Get(tCase.expectedHeader2), tCase.expectedHeaderContent2)
+			}
+
+		})
+	}
+}
+
+func TestHandler_CheckAuthorization(t *testing.T) {
+	// определяем структуру теста
+	// создаём массив тестов: имя и желаемый результат
+	tests := []struct {
+		name                   string
+		inputMetod             string
+		inputEndpoint          string
+		inputBody              string
+		expectedStatusCode     int
+		expectedResponseBody   string
+		expectedHeader2        string
+		expectedHeaderContent2 string
+	}{
+		// определяем все тесты
+		{
+			name:                   "Positive test for user registration",
+			inputMetod:             "POST",
+			inputEndpoint:          "/api/user/register",
+			inputBody:              `{ "login": "dimma", "password": "12345" }`,
+			expectedStatusCode:     http.StatusOK,
+			expectedHeader2:        "Authorization",
+			expectedHeaderContent2: "Bearer",
+		},
+		{
+			name:               "Negative test user registration - wrong JSON",
+			inputMetod:         "POST",
+			inputEndpoint:      "/api/user/register",
+			inputBody:          `{ "login": "dimma, "password": "12345" }`,
+			expectedStatusCode: http.StatusBadRequest,
+		},
+		{
+			name:               "Negative test user registration - login not exist",
+			inputMetod:         "POST",
+			inputEndpoint:      "/api/user/register",
+			inputBody:          `{ "login": "dimma3login", "password": "12345" }`,
+			expectedStatusCode: http.StatusUnauthorized,
+		},
+		{
+			name:               "Negative test user registration - server error",
+			inputMetod:         "POST",
+			inputEndpoint:      "/api/user/register",
+			inputBody:          `{ "login": "dimmaServErr", "password": "12345" }`,
+			expectedStatusCode: http.StatusInternalServerError,
+		},
+	}
+	s := &servicemock.User{}
+	h := handlers.NewUserHandler(s)
+
+	for _, tCase := range tests {
+		// запускаем каждый тест
+		t.Run(tCase.name, func(t *testing.T) {
+			// конфигурирование тестового сервера
+			rout := chi.NewRouter()
+			rout.Post("/api/user/register", h.CheckAuthorization)
+			// конфигурирование запроса
+			request := httptest.NewRequest(tCase.inputMetod, tCase.inputEndpoint, bytes.NewBufferString(tCase.inputBody))
+			// создание запроса
+			w := httptest.NewRecorder()
+			// запуск
+			rout.ServeHTTP(w, request)
+			// оценка результатов
 			assert.Equal(t, tCase.expectedStatusCode, w.Code)
 			if w.Code == http.StatusOK {
 				assert.Contains(t, w.Header().Get(tCase.expectedHeader2), tCase.expectedHeaderContent2)
