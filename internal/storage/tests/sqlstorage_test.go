61c5711c0d29e409c1224743a1a804df47ae7750
dimsonson <104103457+dimsonson@users.noreply.github.com>
2022-11-29 18:41:34 +0300

    sql test start

---
+++ internal/storage/tests/sqlstorage_test.go
@@ -0,0 +1,19 @@
+package sqlstorage_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+)
+
+func TestTodoItemPostgres_Create(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	fmt.Println(mock)
+
+}
